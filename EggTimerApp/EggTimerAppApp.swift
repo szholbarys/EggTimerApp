ff0c97f575ea1008b186793248d0d335295de79a
szholbarys <sakenzholbarys@gmail.com>
2024-10-25 22:10:41 +0500

    Initial Commit

---
+++ EggTimerApp/EggTimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggTimerAppApp.swift
+//  EggTimerApp
+//
+//  Created by Zholbarys on 25.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct EggTimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
